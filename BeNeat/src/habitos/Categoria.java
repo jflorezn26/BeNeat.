b7b44f27546e2a56b8a852021b40c387c2b94cf1
Jeisson <jflorezn@unal.edu.co>
2021-07-28 15:38:09 -0500

    Creación de clases Habitos y Categorias

---
+++ BeNeat/src/habitos/Categoria.java
@@ -0,0 +1,42 @@
+package habitos;
+
+public class Categoria {
+	//Componentes
+		String nombre;
+		String descripcion;
+		String color;
+		
+	//metodos thisConstructor
+		public Categoria (String nom,String desc,String col) {
+			this.nombre=nom;
+			this.descripcion=desc;
+			this.color=col;
+		}
+		public Categoria (String nom,String desc) {
+			this.nombre=nom;
+			this.descripcion=desc;
+			this.color=null;
+		}
+		public Categoria (String nom) {
+			this.nombre=nom;
+			this.descripcion=null;
+			this.color=null;
+		}
+		
+	//metodos get
+		public String getNombre() {
+			return nombre;}
+		public String getDescripcion() {
+			return descripcion;}
+		public String getColor() {
+			return color;}
+		
+	//metodos set
+		public void setNombre(String nombreR) {
+			nombre=nombreR;}
+		public void setDescripcion(String descripcionR) {
+			descripcion=descripcionR;}
+		public void setColor(String colorR) {
+			color=colorR;}
+		
+	}
--- BeNeat/src/paquette/Jajsla.java
+++
@@ -1,5 +0,0 @@
-package paquette;
-
-public class Jajsla {
-
-}
