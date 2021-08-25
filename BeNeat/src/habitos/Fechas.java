9e3356f7ed35ae179ba1fd0a9c99d0fabaa8246b
jflorezn26 <87783380+jflorezn26@users.noreply.github.com>
2021-08-25 16:04:51 -0500

    ...
    ...

--- BeNeat/src/habitos/Fechas.java
+++ BeNeat/src/habitos/Fechas.java
@@ -6,102 +6,140 @@ import java.util.GregorianCalendar;
  * Tan pronto se realicen todos los ajustes, se creará el método para que sea ejecutada 
  * desde la clase principal (App)
  */
+
 public class Fechas {
 
-	static Dia año [][] =  new Dia[12][31];
+	Dia año[][] = new Dia[12][31];
+
 	Fechas() {
-		for (int i = 0; i< año.length  ;i++) {
-		
-		for (int j = 0; j <año[i].length; j++) {
-		
-		año[i][j] = new Dia ();
-		if (i==0||i==2||i==4||i==6||i==7||i==9||i==11) {
-		año[i][j].fecha = new GregorianCalendar (2020,i,j+1); 
+		for (int i = 0; i < año.length; i++) {
+
+			for (int j = 0; j < año[i].length; j++) {
+
+				año[i][j] = new Dia();
+				if (i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11) {
+					año[i][j].fecha = new GregorianCalendar(2020, i, j + 1);
+				}
+				if (i == 1) {
+					if (j < 28)
+						año[i][j].fecha = new GregorianCalendar(2020, i, j + 1);
+					else
+						año[i][j].fecha = null;
+				}
+				if (i == 3 || i == 5 || i == 8 || i == 10) {
+					if (j < 30)
+						año[i][j].fecha = new GregorianCalendar(2020, i, j + 1);
+					else
+						año[i][j].fecha = null;
+				}
+
+			}
 		}
-		if(i==1) {
-		if (j<28)
-			año[i][j].fecha = new GregorianCalendar (2020,i,j+1);
-		else
-			año[i][j].fecha = null;
-		}
-		if (i==3||i==5||i==8||i==10) {
-		if (j<30)
-			año[i][j].fecha = new GregorianCalendar (2020,i,j+1);
-		else
-			año[i][j].fecha = null;
-		}
-		
 	}
- }
-}
+
 	public static void main(String[] args) {
-		final Fechas agenda = new Fechas();
-    	agenda.imprimirFechas();
+
+		Fechas agenda = new Fechas();
+		//agenda.imprimirFechas();
+		String texto[][] = agenda.fechasATexto();
+
+		for (int i = 0; i < texto.length; i++) {
+
+			for (int j = 0; j < texto[i].length; j++) {
+				
+				System.out.println(texto[i][j]);
+
+			}
+			
+		}
+		System.out.println(texto[2][13]+":)");
 	}
-	
- void imprimirFechas() {
-		for (int i = 0; i< año.length  ;i++) {
-			switch( i) {
+
+	void imprimirFechas() {
+		for (int i = 0; i < año.length; i++) {
+			switch (i) {
 			case 0:
-				System.out.println("Enero"); break;
+				System.out.println("Enero");
+				break;
 			case 1:
 				System.out.println("Febrero");
 				break;
-			case 2 : 
-				System.out.println("Marzo");break;
+			case 2:
+				System.out.println("Marzo");
+				break;
 			case 3:
-				System.out.println ("Abril");
+				System.out.println("Abril");
 				break;
 			case 4:
-				System.out.println("Mayo");break;
+				System.out.println("Mayo");
+				break;
 			case 5:
-				System.out.println ("Junio");
+				System.out.println("Junio");
 				break;
 			case 6:
-				System.out.println("Julio"); break;
+				System.out.println("Julio");
+				break;
 			case 7:
-				System.out.println("Agosto"); break;
+				System.out.println("Agosto");
+				break;
 			case 8:
-				System.out.println ("Septiembre");
+				System.out.println("Septiembre");
 				break;
 			case 9:
-				System.out.println("Octubre"); break;
+				System.out.println("Octubre");
+				break;
 			case 10:
-				System.out.println ("Noviembre");
+				System.out.println("Noviembre");
 				break;
 			case 11:
-				System.out.println("Diciembre");break;}
-			
-		for (int j = 0; j <año[i].length; j++) {
-			if(año[i][j].fecha != null) {
-				System.out.println();
-				System.out.println(año[i][j].fecha.get(Calendar.DATE));	
-				if (año[i][j].contador==-1)
-					System.out.println("Nada programado para este día :)");
-				
-				System.out.println();
+				System.out.println("Diciembre");
+				break;
 			}
+
+			for (int j = 0; j < año[i].length; j++) {
+				if (año[i][j].fecha != null) {
+					System.out.println();
+					System.out.println(año[i][j].fecha.get(Calendar.DATE));
+					if (año[i][j].contador == -1)
+						System.out.println("Nada programado para este día :)");
+
+					System.out.println();
+				}
 			}
-			
+
 		}
-		
-		
+
 	}
-		
-}
+
+	String dia(int x) {
+		return Integer.toString(x);
+	}
+
+	int dianum(int i, int j) {
+		int a = año[i][j].fecha.get(Calendar.DATE);
+		return a;
+	}
+
+	String[][] fechasATexto() {
+
+		String texto[][] = new String[12][31];
+		for (int i = 0; i < texto.length; i++) {
+
+			for (int j = 0; j < texto[i].length; j++) {
+				if (año[i][j].fecha != null)
+					texto[i][j] = Integer.toString(año[i][j].fecha.get(Calendar.DATE));
+				else 
+					texto[i][j] = ".";
+
+			}
+
+		}
+
+		return texto;
+
+	}
+	
+	
 	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
